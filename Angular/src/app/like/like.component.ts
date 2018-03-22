836fe7ab0d91813eaedc20df76e269e5b610da11
j-colson3 <jcolson419@gmail.com>
2018-03-22 18:26:58 -0400

    Completed Like Component, fixed Limb styling

--- Angular/src/app/like/like.component.ts
+++ Angular/src/app/like/like.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-like',
@@ -7,9 +7,49 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LikeComponent implements OnInit {
 
+
+  @Input() count;
+  temp: string;
+  clickCount: number;
+  color: string;
+
   constructor() { }
 
   ngOnInit() {
+    this.clickCount = 0;
+    this.updateIcon(this.count);
+    
+  }
+
+  changeCount() {
+    this.clickCount++;
+    if(this.clickCount % 2 == 1){
+      this.count += 1;
+    } else {
+      this.count -= 1;
+    }
+    this.updateIcon(this.count);
+    console.log(this.clickCount);
+    console.log(this.count);
+  }
+
+  updateIcon(numCount: number) {
+    if(numCount == 0){
+      this.temp = 'fa fa-thermometer-empty'; 
+      this.color = 'color1';
+    } else if (numCount == 1) {
+      this.temp = 'fa fa-thermometer-quarter';
+      this.color = 'color2'; 
+    } else if (numCount == 2) {
+      this.temp = 'fa fa-thermometer-half'; 
+      this.color = 'color3';
+    } else if (numCount == 3) {
+      this.temp = 'fa fa-thermometer-three-quarters'; 
+      this.color = 'color4';
+    } else {
+      this.temp = 'fa fa-thermometer-full'; 
+      this.color = 'color5';
+    }
   }
 
 }
--- Angular/src/app/limb/limb.component.ts
+++ Angular/src/app/limb/limb.component.ts
@@ -8,9 +8,15 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class LimbComponent implements OnInit {
 
+  count: number = 0; //This will be the number of likes
+  name: string = 'My Name';
+  timeStamp: any = '1/01/00 00:00 AM';
+
+
   constructor() { }
 
   ngOnInit() {
+
   }
 
 }
