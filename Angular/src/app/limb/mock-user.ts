d02e14f18d4ce31fe83e9b85512d162c1a0f9166
Scott Bennett <scott.g.bennett@gmail.com>
2018-03-25 13:06:42 -0400

    Bye, felicia

--- Angular/src/app/limb/mock-user.ts
+++
@@ -1,8 +0,0 @@
-export class User {
-    username: string = "GStar"; //This will be the number of likes
-    firstName: string = "Gilbert";
-    lastName: string = 'Cheese';
-    email: string = "scott.g.bennett@gmail.com";
-    //email: string = 'scott.g.bennett@gmail.com';
-    bdate: string = null;
-}
