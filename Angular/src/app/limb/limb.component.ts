29baac13eff9321c75323080f70b8a48427a4809
Scott Bennett <scott.g.bennett@gmail.com>
2018-03-27 11:05:55 -0400

    add same changes as zain

--- Angular/src/app/limb/limb.component.ts
+++ Angular/src/app/limb/limb.component.ts
@@ -33,6 +33,8 @@ export class LimbComponent implements OnInit {
       this.user = <User> res;
           if(this.user.profilePic)
             this.url=this.user.profilePic;
+          else
+            this.url = '../../assets/images/user-200';
           console.log(this.url+" of "+this.user.firstName)
           console.log("User:"+JSON.stringify(this.user));
     });
