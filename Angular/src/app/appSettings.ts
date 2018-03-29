5903fc99ca6c6ddf235239535e78feb4f0734a6d
Scott Bennett <scott.g.bennett@gmail.com>
2018-03-28 22:34:08 -0400

    BUUUUIIIILLLLDDDD

--- Angular/src/app/appSettings.ts
+++ Angular/src/app/appSettings.ts
@@ -1,6 +1,6 @@
 export class appSettings {
 
-    public static ANGULAR_URL: string = 'http://localhost:4200/';
-    public static BACKEND_URL: string = 'http://localhost:666/';
+    public static ANGULAR_URL: string = 'http://35.185.104.21:8085/';
+    public static BACKEND_URL: string = 'http://35.185.104.21:8085/';
 
 }
